daa945ec9612187e6db61489928e3a86ff333f59
Chad Dattilio <chad.dattilio@slalom.com>
2020-12-13 13:26:04 -0500

    more updates

--- src/App.js
+++ src/App.js
@@ -21,54 +21,68 @@ const App = () => {
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleSearch = event => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
  
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
  
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 };
 
 const Search = props => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-
-    props.onSearch(event);
-  };
+  const {search, onSearch} = props;
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
+      <input 
+        id="search" 
+        type="text" 
+        value={search} 
+        onChange={onSearch} />
     </div>
   );
 };
 
-const List = props =>
-  props.list.map(item => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  ));
+const List = ({ list }) =>
+  list.map(item => <Item key={item.objectID} item={item} />);
+
+const Item = ({ item }) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>
+);
+
+// const List = props =>
+//   props.list.map(item => (
+//     <div key={item.objectID}>
+//       <span>
+//         <a href={item.url}>{item.title}</a>
+//       </span>
+//       <span>{item.author}</span>
+//       <span>{item.num_comments}</span>
+//       <span>{item.points}</span>
+//     </div>
+//   ));
 
 export default App;
